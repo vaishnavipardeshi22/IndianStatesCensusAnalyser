f4cacccdff3d666d3cb86200a152df3ebb8c92e7
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-17 15:38:19 +0530

    ADDED : Test method to throw custom exception for incorrect file

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.statecensusanalysertest;
 
+import com.bridgelabz.exception.StateCensusAnalyserException;
 import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,15 +11,26 @@ public class StateCensusAnalyserTest {
 
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
     private static final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
+    private static final String CSV_FILE_INCORRECT_PATH = "src/test/resources/stateCensusData.csv";
 
     @Test
     public void givenIndianStatesCensusCSVFile_WhenMatchNoOfRecord_ThenReturnTrue() {
         try {
             int numberOfRecord = stateCensusAnalyser.loadCSVDataFile(CSV_FILE_PATH);
             Assert.assertEquals(29, numberOfRecord);
-        }
-        catch (IOException e){
+        } catch (IOException | StateCensusAnalyserException e) {
             e.getStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIncorrectIndianStatesCensusCSVFile_WhenMatchNoOfRecord_ThenThrowCustomException() throws StateCensusAnalyserException {
+        try {
+            int numberOfRecord = stateCensusAnalyser.loadCSVDataFile(CSV_FILE_INCORRECT_PATH);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

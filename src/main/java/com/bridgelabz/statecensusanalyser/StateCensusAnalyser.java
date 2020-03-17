69c7b678ff2b458c3b833c634c5b996b76323279
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-17 16:12:20 +0530

    REFACTOR : Function to handle custom exception for incorrect file

--- src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
@@ -1,19 +1,20 @@
 package com.bridgelabz.statecensusanalyser;
 
+import com.bridgelabz.exception.StateCensusAnalyserException;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
 
     //FUNCTION TO LOAD CSV DATA AND COUNT NUMBER OF RECORDS IN CSV FILE
-    public int loadCSVDataFile(String csvFilePath) throws IOException {
+    public int loadCSVDataFile(String csvFilePath) throws IOException, StateCensusAnalyserException {
         int numberOfRecords = 0;
         try (
                 Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
@@ -28,6 +29,8 @@ public class StateCensusAnalyser {
                 numberOfRecords++;
                 csvStateCensusIterator.next();
             }
+        } catch (NoSuchFileException e) {
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, "Incorrect file.");
         }
         return numberOfRecords;
     }

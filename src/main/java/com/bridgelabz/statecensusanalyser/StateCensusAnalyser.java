a3446d5b46f76cd7873f2f9a4834aee5e8928d93
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-17 11:56:46 +0530

    ADDED : Function to load the CSV file and get number of records

--- src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
@@ -1,6 +1,38 @@
 package com.bridgelabz.statecensusanalyser;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class StateCensusAnalyser {
+
+    //FUNCTION TO LOAD CSV DATA AND COUNT NUMBER OF RECORDS IN CSV FILE
+    public int loadCSVDataFile(String csvFilePath) throws IOException {
+        int numberOfRecords = 0;
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+        ) {
+            CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVStateCensus.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVStateCensus> csvStateCensusIterator = csvToBean.iterator();
+
+            while (csvStateCensusIterator.hasNext()) {
+                numberOfRecords++;
+                csvStateCensusIterator.next();
+            }
+        }
+        return numberOfRecords;
+    }
+
+    //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("******************** WELCOME TO INDIAN STATES CENSUS ANALYSER ********************");
     }

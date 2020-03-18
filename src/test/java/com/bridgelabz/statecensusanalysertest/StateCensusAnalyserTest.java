bcd5681f57924b17be81c4d48d242c768927df9f
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-18 16:06:52 +0530

    ADDED : Test method to throw custom exception for incorrect delimiter

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -8,13 +8,15 @@ import org.junit.Test;
 public class StateCensusAnalyserTest {
 
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-    private static final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
-    private static final String CSV_FILE_INCORRECT_PATH = "src/test/resources/stateCensusData.csv";
-    private static final String CSV_INCORRECT_FILE_TYPE = "src/test/resources/stateCensusData.pdf";
+    private static final String STATE_CENSUS_ANALYSER_CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
+    private static final String STATE_CENSUS_ANALYSER_CSV_FILE_INCORRECT_PATH = "src/test/resources/stateCensusData.csv";
+    private static final String STATE_CENSUS_ANALYSER_CSV_INCORRECT_FILE_TYPE = "src/test/resources/stateCensusData.pdf";
+    private static final String STATE_CENSUS_ANALYSER_CSV_INCORRECT_DELIMITER_FILE_TYPE = "src/test/resources/StateCensusDataIncorrectDelimiter.csv";
+
     @Test
     public void givenIndianStatesCensusCSVFile_WhenMatchNoOfRecord_ThenReturnTrue() {
         try {
-            int numberOfRecord = stateCensusAnalyser.loadCSVDataFile(CSV_FILE_PATH);
+            int numberOfRecord = stateCensusAnalyser.loadCSVDataFile(STATE_CENSUS_ANALYSER_CSV_FILE_PATH);
             Assert.assertEquals(29, numberOfRecord);
         } catch (StateCensusAnalyserException e) {
             e.getStackTrace();
@@ -22,20 +24,29 @@ public class StateCensusAnalyserTest {
     }
 
     @Test
-    public void givenIncorrectIndianStatesCensusCSVFile_WhenMatchNoOfRecord_ThenThrowCustomException() {
+    public void givenIncorrectIndianStatesCensusCSVFile_WhenUnmatch_ThenThrowCustomException() {
         try {
-            stateCensusAnalyser.loadCSVDataFile(CSV_FILE_INCORRECT_PATH);
+            stateCensusAnalyser.loadCSVDataFile(STATE_CENSUS_ANALYSER_CSV_FILE_INCORRECT_PATH);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
         }
     }
 
     @Test
-    public void givenIncorrectIndianStatesCensusCSVFileType_WhenMatchNoOfRecord_ThenThrowCustomException() {
+    public void givenIncorrectIndianStatesCensusCSVFileType_WhenUnmatch_ThenThrowCustomException() {
         try {
-            int numberOfRecord = stateCensusAnalyser.loadCSVDataFile(CSV_INCORRECT_FILE_TYPE);
+            int numberOfRecord = stateCensusAnalyser.loadCSVDataFile(STATE_CENSUS_ANALYSER_CSV_INCORRECT_FILE_TYPE);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_TYPE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIncorrectDelimiterIndianStatesCensusCSVFile_WhenUnmatch_ThenThrowCustomException() {
+        try {
+            stateCensusAnalyser.loadCSVDataFile(STATE_CENSUS_ANALYSER_CSV_INCORRECT_DELIMITER_FILE_TYPE);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_DELIMITER, e.type);
         }
     }
 }

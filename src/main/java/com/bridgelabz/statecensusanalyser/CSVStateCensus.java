3897988e79faf36f617e55ec1812bce47eff25a8
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-18 16:17:05 +0530

    REFACTOR : Annotations parameter in POJO class

--- src/main/java/com/bridgelabz/statecensusanalyser/CSVStateCensus.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/CSVStateCensus.java
@@ -3,15 +3,15 @@ package com.bridgelabz.statecensusanalyser;
 import com.opencsv.bean.CsvBindByName;
 
 public class CSVStateCensus {
-    @CsvBindByName(column = "State")
+    @CsvBindByName(column = "State", required = true)
     private String state;
 
-    @CsvBindByName(column = "Population")
+    @CsvBindByName(column = "Population", required = true)
     private String population;
 
-    @CsvBindByName(column = "AreaInSqKm")
+    @CsvBindByName(column = "AreaInSqKm", required = true)
     private String areaInSqKm;
 
-    @CsvBindByName(column = "DensityPerSqKm")
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
     private String densityPerSqKm;
 }

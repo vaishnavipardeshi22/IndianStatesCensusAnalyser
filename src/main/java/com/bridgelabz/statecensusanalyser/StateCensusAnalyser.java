9b2b559b43d45b93f6bed6062f6bc66f7082703a
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-19 10:11:47 +0530

    REFACTOR : Function to handle incorrect header exception

--- src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
@@ -13,6 +13,11 @@ import java.util.Iterator;
 
 public class StateCensusAnalyser {
 
+    //MAIN METHOD
+    public static void main(String[] args) {
+        System.out.println("******************** WELCOME TO INDIAN STATES CENSUS ANALYSER ********************");
+    }
+
     //FUNCTION TO LOAD CSV DATA AND COUNT NUMBER OF RECORDS IN CSV FILE
     public int loadCSVDataFile(String csvFilePath) throws StateCensusAnalyserException {
         int numberOfRecords = 0;
@@ -23,7 +28,7 @@ public class StateCensusAnalyser {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader)
                     .withType(CSVStateCensus.class)
-                    .withIgnoreLeadingWhiteSpace(false)
+                    .withIgnoreLeadingWhiteSpace(true)
                     .build();
             Iterator<CSVStateCensus> csvStateCensusIterator = csvToBean.iterator();
 
@@ -32,17 +37,12 @@ public class StateCensusAnalyser {
                 csvStateCensusIterator.next();
             }
         } catch (RuntimeException e) {
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_DELIMITER, "Incorrect file.");
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_DELIMITER_OR_HEADER, "Incorrect file.");
         } catch (NoSuchFileException e) {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, "Incorrect file.");
         } catch (IOException e) {
             e.getStackTrace();
         }
         return numberOfRecords;
-    }
-
-    //MAIN METHOD
-    public static void main(String[] args) {
-        System.out.println("******************** WELCOME TO INDIAN STATES CENSUS ANALYSER ********************");
     }
 }

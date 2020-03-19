37995a0b511c70b624db7be10d4e9514137bc8d8
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-19 12:26:53 +0530

    ADDED : Test methhod to throw custom exception for incorrect file type of state code csv file

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -5,17 +5,16 @@ import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
-
 public class StateCensusAnalyserTest {
 
     private static final String STATE_CENSUS_DATA_CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
     private static final String STATE_CENSUS_DATA_CSV_FILE_INCORRECT_PATH = "src/test/resources/stateCensusData.csv";
-    private static final String STATE_CENSUS_DATA_CSV_INCORRECT_FILE_TYPE = "src/test/resources/stateCensusData.pdf";
+    private static final String STATE_CENSUS_DATA_CSV_INCORRECT_FILE_TYPE = "src/test/resources/StateCensusData.pdf";
     private static final String STATE_CENSUS_DATA_CSV_INCORRECT_DELIMITER_FILE_TYPE = "src/test/resources/StateCensusDataIncorrectDelimiter.csv";
     private static final String STATE_CENSUS_DATA_CSV_INCORRECT_HEADER_FILE = "src/test/resources/StateCensusDataIncorrectHeader.csv";
     private static final String STATE_CODE_DATA_CSV_FILE_PATH = "src/test/resources/StateCode.csv";
     private static final String STATE_CODE_DATA_CSV_FILE_INCORRECT_PATH = "src/test/resources/stateCode.csv";
+    private static final String STATE_CODE_DATA_CSV_FILE_INCORRECT_TYPE = "src/test/resources/StateCode.pdf";
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 
     @Test
@@ -66,12 +65,12 @@ public class StateCensusAnalyserTest {
 
     @Test
     public void givenIndianStatesCodeCSVFile_WhenMatchNoOfRecord_ThenReturnTrue() {
-            try {
-                int numberOfRecord = stateCensusAnalyser.loadCSVDataFileForStateCodeData(STATE_CODE_DATA_CSV_FILE_PATH);
-                Assert.assertEquals(37, numberOfRecord);
-            } catch (StateCensusAnalyserException e) {
-                e.printStackTrace();
-            }
+        try {
+            int numberOfRecord = stateCensusAnalyser.loadCSVDataFileForStateCodeData(STATE_CODE_DATA_CSV_FILE_PATH);
+            Assert.assertEquals(37, numberOfRecord);
+        } catch (StateCensusAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -83,5 +82,13 @@ public class StateCensusAnalyserTest {
         }
     }
 
+    @Test
+    public void givenIncorrectIndianStatesCodeCSVFileType_WhenUnmatch_ThenThrowCustomException() {
+        try {
+            int numberOfRecord = stateCensusAnalyser.loadCSVDataFileForStateCodeData(STATE_CODE_DATA_CSV_FILE_INCORRECT_TYPE);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_TYPE, e.type);
+        }
+    }
 }
 

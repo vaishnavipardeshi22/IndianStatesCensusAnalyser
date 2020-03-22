17b350ceb0cafbd0fef6be4df1790f4df6135315
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-22 21:07:06 +0530

    REFACTOR : Function throws CSVBuilderException

--- src/main/java/com/bridgelabz/statecensusanalyser/OpenCSVBuilder.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/OpenCSVBuilder.java
@@ -1,19 +1,24 @@
 package com.bridgelabz.statecensusanalyser;
 
+import com.bridgelabz.exception.CSVBuilderException;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
 
-public class OpenCSVBuilder implements ICSVBuilder {
-    public <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass) {
-        CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder(reader)
-                .withType(csvClass)
-                .withIgnoreLeadingWhiteSpace(true);
-        CsvToBean<T> csvToBean = csvToBeanBuilder.build();
-        Iterator<T> csvCensusIterator = csvToBean.iterator();
-        return csvCensusIterator;
+public class OpenCSVBuilder<T> implements ICSVBuilder {
+    public <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass) throws CSVBuilderException {
+        try {
+            CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder(reader)
+                    .withType(csvClass)
+                    .withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<T> csvToBean = csvToBeanBuilder.build();
+            Iterator<T> csvCensusIterator = csvToBean.iterator();
+            return csvCensusIterator;
+        } catch (IllegalStateException e) {
+            throw new CSVBuilderException(CSVBuilderException.ExceptionType.NO_SUCH_FILE,
+                    "Incorrect file.");
+        }
     }
-
 }

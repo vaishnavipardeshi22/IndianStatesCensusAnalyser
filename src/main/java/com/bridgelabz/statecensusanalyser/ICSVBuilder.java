9528965ee0f9f6e54f448e25ad5a2a4335808190
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-22 20:27:58 +0530

    REFACTOR : Throws CSVBuilderException

--- src/main/java/com/bridgelabz/statecensusanalyser/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/ICSVBuilder.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.statecensusanalyser;
 
+import com.bridgelabz.exception.CSVBuilderException;
+
 import java.io.Reader;
 import java.util.Iterator;
 
 public interface ICSVBuilder {
-    public <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass);
+    public <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass) throws CSVBuilderException;
 }

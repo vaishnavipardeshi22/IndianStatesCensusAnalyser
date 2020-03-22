0777f1e2150b435ec661fce28272fde6a9754809
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-22 23:06:16 +0530

    ADDED : Usage of getCSVList using List

--- src/main/java/com/bridgelabz/statecensusanalyser/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/ICSVBuilder.java
@@ -4,7 +4,9 @@ import com.bridgelabz.exception.CSVBuilderException;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder {
-    public <T> Iterator<T> getCSVIterator(Reader reader, Class<T> csvClass) throws CSVBuilderException;
+    public <T> Iterator<T> getCSVIterator(Reader reader, Class csvClass) throws CSVBuilderException;
+    public List getCSVList(Reader reader, Class csvClass) throws CSVBuilderException;
 }

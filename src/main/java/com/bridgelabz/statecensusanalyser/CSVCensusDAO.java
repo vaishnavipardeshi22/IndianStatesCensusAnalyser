35497f240a83847b486fc5f68e28dd8c7a93562d
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-29 19:10:37 +0530

    ADDED : Single CensusDAO for both Indian census CSV and US census CSV

---
+++ src/main/java/com/bridgelabz/statecensusanalyser/CSVCensusDAO.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.statecensusanalyser;
+
+public class CSVCensusDAO {
+
+    public String state;
+    public String stateCode;
+    public double areaInSqKm;
+    public double densityPerSqKm;
+    public long population;
+
+    public CSVCensusDAO(CSVStateCensus indianCensusCSV) {
+        this.state = indianCensusCSV.state;
+        this.areaInSqKm = indianCensusCSV.areaInSqKm;
+        this.densityPerSqKm = indianCensusCSV.densityPerSqKm;
+        this.population = indianCensusCSV.population;
+    }
+
+    public CSVCensusDAO(CSVUSCensus usCensusCSV) {
+        this.state = usCensusCSV.usState;
+        this.areaInSqKm = usCensusCSV.totalArea;
+        this.densityPerSqKm = usCensusCSV.populationDensity;
+        this.population = usCensusCSV.usPopulation;
+    }
+}

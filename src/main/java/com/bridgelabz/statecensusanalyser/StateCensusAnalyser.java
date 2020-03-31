534cba0fa2c2fc2ba2b5334a68757d83bb3c3c4c
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-31 17:06:12 +0530

    REFACTOR : Code for generic sorting method

--- src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/statecensusanalyser/StateCensusAnalyser.java
@@ -3,16 +3,20 @@ package com.bridgelabz.statecensusanalyser;
 import com.bridgelabz.exception.StateCensusAnalyserException;
 import com.google.gson.Gson;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StateCensusAnalyser {
 
     List<CSVCensusDAO> csvCensusDAOList = null;
     Map<String, CSVCensusDAO> csvStateCensusDAOMap = null;
+    private Country country;
 
-    public StateCensusAnalyser() {
-        this.csvStateCensusDAOMap = new HashMap<>();
+    //CONSTRUCTOR
+    public StateCensusAnalyser(Country country) {
+        this.country = country;
     }
 
     //MAIN METHOD
@@ -20,73 +24,28 @@ public class StateCensusAnalyser {
         System.out.println("******************** WELCOME TO INDIAN STATES CENSUS ANALYSER ********************");
     }
 
+    //METHOD TO LOAD CENSUS DATA
     public int loadStateCensusCSVData(Country country, String... csvFilePath) throws StateCensusAnalyserException {
         csvStateCensusDAOMap = CensusAdapterFactory.getCensusData(country, csvFilePath);
         csvCensusDAOList = csvStateCensusDAOMap.values().stream().collect(Collectors.toList());
         return csvStateCensusDAOMap.size();
     }
 
-    //FUNCTION FOR SORTING
-    private <T> void sortCSVData(Comparator<T> censusCSVComparator, List<T> csvList) {
-        for (int index1 = 0; index1 < csvList.size() - 1; index1++) {
-            for (int index2 = 0; index2 < csvList.size() - index1 - 1; index2++) {
-                T census1 = csvList.get(index2);
-                T census2 = csvList.get(index2 + 1);
-                if (censusCSVComparator.compare(census1, census2) > 0) {
-                    csvList.set(index2, census2);
-                    csvList.set(index2 + 1, census1);
-                }
-            }
-        }
-    }
-
-    //FUNCTION TO SORT STATE CENSUS DATA
-    public String getSortedStateWiseCensusData() throws StateCensusAnalyserException {
-        if (csvStateCensusDAOMap == null || csvStateCensusDAOMap.size() == 0)
+    //FUNCTION FOR SORT CENSUS DATA
+    public String getSortedCensusData(SortingMode mode) throws StateCensusAnalyserException {
+        if (csvCensusDAOList == null || csvCensusDAOList.size() == 0)
             throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_CENSUS_DATA, "No census data");
-        Comparator<CSVCensusDAO> censusCSVComparator = Comparator.comparing(csvStateCensus -> csvStateCensus.state);
-        List<CSVCensusDAO> censusDAOList = csvStateCensusDAOMap.values().stream().collect(Collectors.toList());
-        this.sortCSVData(censusCSVComparator, censusDAOList);
-        String sortedStateCensusJson = new Gson().toJson(censusDAOList);
-        return sortedStateCensusJson;
+        ArrayList arrayList = csvStateCensusDAOMap.values().stream()
+                .sorted(CSVCensusDAO.getSortComparator(mode))
+                .map(csvCensusDAO -> csvCensusDAO.getCensusDTO(country))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return new Gson().toJson(arrayList);
     }
 
-    //FUNCTION TO SORT CENSUS DATA BY POPULATION
-    public String getSortedPopulationWiseCensusData() throws StateCensusAnalyserException {
-        if (csvStateCensusDAOMap == null || csvStateCensusDAOMap.size() == 0)
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_CENSUS_DATA, "No census data");
-        Comparator<CSVCensusDAO> censusCSVComparator = Comparator.comparing(csvStateCensus -> csvStateCensus.population);
-        List<CSVCensusDAO> censusDAOList = csvStateCensusDAOMap.values().stream().collect(Collectors.toList());
-        this.sortCSVData(censusCSVComparator, censusDAOList);
-        Collections.reverse(censusDAOList);
-        String sortedStateCensusJson = new Gson().toJson(censusDAOList);
-        return sortedStateCensusJson;
-    }
-
-    //FUNCTION TO SORT CENSUS DATA BY POPULATION DENSITY
-    public String getSortedPopulationDensityWiseCensusData() throws StateCensusAnalyserException {
-        if (csvStateCensusDAOMap == null || csvStateCensusDAOMap.size() == 0)
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_CENSUS_DATA, "No census data");
-        Comparator<CSVCensusDAO> censusCSVComparator = Comparator.comparing(csvStateCensus -> csvStateCensus.densityPerSqKm);
-        List<CSVCensusDAO> censusDAOList = csvStateCensusDAOMap.values().stream().collect(Collectors.toList());
-        this.sortCSVData(censusCSVComparator, censusDAOList);
-        Collections.reverse(censusDAOList);
-        String sortedStateCensusJson = new Gson().toJson(censusDAOList);
-        return sortedStateCensusJson;
-    }
-
-    //FUNCTION TO SORT CENSUS DATA BY AREA
-    public String getSortedStateAreaWiseCensusData() throws StateCensusAnalyserException {
-        if (csvStateCensusDAOMap == null || csvStateCensusDAOMap.size() == 0)
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_CENSUS_DATA, "No census data");
-        Comparator<CSVCensusDAO> censusCSVComparator = Comparator.comparing(csvStateCensus -> csvStateCensus.areaInSqKm);
-        List<CSVCensusDAO> censusDAOList = csvStateCensusDAOMap.values().stream().collect(Collectors.toList());
-        this.sortCSVData(censusCSVComparator, censusDAOList);
-        Collections.reverse(censusDAOList);
-        String sortedStateCensusJson = new Gson().toJson(censusDAOList);
-        return sortedStateCensusJson;
-    }
-
+    //ENUM FOR COUNTRY
     public enum Country {INDIA, US}
+
+    //ENUM FOR SORTING MODE
+    public enum SortingMode {STATE, POPULATION, AREA, DENSITY}
 }
 
